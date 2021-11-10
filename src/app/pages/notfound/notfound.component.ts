7fe848f7e30e9c5323e57a82c430dff98adca613
Alberto Ogayar <alogayar@gmail.com>
2021-11-11 00:45:27 +0100

    nuevo login y register

--- src/app/pages/notfound/notfound.component.ts
+++ src/app/pages/notfound/notfound.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-notfound',
   templateUrl: './notfound.component.html',
   styleUrls: ['./notfound.component.css']
 })
-export class NotfoundComponent implements OnInit {
+export class NotfoundComponent {
+
+  year = new Date().getFullYear();
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+ 
 
 }
