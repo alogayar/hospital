9c9e7745372e221c8ab6934d2f743de7ef3e4a1e
Alberto Ogayar <alogayar@gmail.com>
2021-11-15 00:16:42 +0100

    graficas ok

--- src/app/pages/grafica1/grafica1.component.ts
+++ src/app/pages/grafica1/grafica1.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ChartType } from 'chart.js';
+import { MultiDataSet, Label } from 'ng2-charts/ng2-charts';
+
+
+
 
 @Component({
   selector: 'app-grafica1',
   templateUrl: './grafica1.component.html',
-  styleUrls: ['./grafica1.component.css']
+  styleUrls: ['./grafica1.component.css'],
 })
-export class Grafica1Component implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class Grafica1Component {
+  public doughnutChartLabels: Label[] = [
+    'Download Sales',
+    'In-Store Sales',
+    'Mail-Order Sales',
+  ];
+  public doughnutChartData: MultiDataSet = [
+    [350, 450, 100],
+    [50, 150, 120],
+    [250, 130, 70],
+  ];
+  public doughnutChartType: ChartType = 'doughnut';
 }
